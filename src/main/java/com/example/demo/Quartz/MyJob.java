f7438939c37bf41a5aeda92cc07812343fac9c80
vidroid16 <shalyaandrey16@gmail.com>
2021-10-25 04:59:16 +0300

    test refactor

--- src/main/java/com/example/demo/Quartz/MyJob.java
+++ src/main/java/com/example/demo/Quartz/MyJob.java
@@ -1,20 +1,16 @@
 package com.example.demo.Quartz;
-
 import com.example.demo.Services.Implementations.TestServiceImpl;
-import com.example.demo.Services.TestService;
-import lombok.extern.slf4j.Slf4j;
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
-@Slf4j
-public class MyJob implements Job {
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@EnableScheduling
+public class MyJob{
     @Autowired
     private TestServiceImpl memberService;
-    @Override
-    public void execute(JobExecutionContext context) {
-        log.info("Job ** {} ** starting @ {}", context.getJobDetail().getKey().getName(), context.getFireTime());
+    @Scheduled(fixedRate = 30000)
+    public void execute() {
         System.out.println(memberService.say());
         memberService.mail();
-        log.info("Job ** {} ** completed.  Next job scheduled @ {}", context.getJobDetail().getKey().getName(), context.getNextFireTime());
     }
 }
