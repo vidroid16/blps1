57e5c26d2b9b2f34f7b57b6a1cd1074665df9753
vidroid16 <shalyaandrey16@gmail.com>
2021-10-25 16:04:22 +0300

    test refactor

--- src/main/java/com/example/demo/Quartz/MyJob.java
+++ src/main/java/com/example/demo/Quartz/MyJob.java
@@ -1,5 +1,7 @@
 package com.example.demo.Quartz;
-import com.example.demo.Services.Implementations.TestServiceImpl;
+import com.example.demo.DataBase.UsersDB.User;
+import com.example.demo.DataBase.UsersDB.UsersRepository;
+import com.example.demo.Quartz.Mail.MailSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -7,10 +9,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableScheduling
 public class MyJob{
     @Autowired
-    private TestServiceImpl memberService;
+    private UsersRepository usersRepository;
     @Scheduled(fixedRate = 30000)
     public void execute() {
-        System.out.println(memberService.say());
-        memberService.mail();
+        MailSender mailSender = new MailSender();
+        for (User u:usersRepository.getall()) {
+            mailSender.send("Kickstarter: Лучие проекты за неделю для вас!!", "У нас 1 проект вы о чем вообще! Но зато есть анекдот", u.getLogin());
+        }
     }
 }
--- src/main/java/com/example/demo/Services/TestService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Services;
-
-public interface TestService {
-    public String say();
-    public void use();
-    public void mail();
-}
